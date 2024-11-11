198c5dd5696d3dfd0cce2265b9606e82b12284ea
Demeure.Corentin <160240483+etu51424@users.noreply.github.com>
2024-11-11 14:08:15 +0100

    initDB garni

--- initDB.sql
+++ initDB.sql
@@ -1,1 +1,30 @@
+USE java_webapp_db;
 
+CREATE TABLE `user` (
+	ID varchar(255) primary key,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255),
+    Country varchar(255)
+);
+
+CREATE TABLE `command` (
+	ID varchar(255) primary key,
+
+);
+
+CREATE TABLE `commandLine` (
+	lineNumber int,
+	commandID varchar(255),
+	primary key (lineNumber, commandID)
+);
+
+CREATE TABLE `article` (
+	ID varchar(255) primary key,
+	
+
+);
+    
+    
+    
