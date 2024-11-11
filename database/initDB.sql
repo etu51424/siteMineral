44afd65410892a09d435fe261d067c66bf245188
xavierDujardinn <160240062+xavierDujardinn@users.noreply.github.com>
2024-11-11 15:07:18 +0100

    Update initDB.sql

--- database/initDB.sql
+++ database/initDB.sql
@@ -1,14 +1,16 @@
 USE java_webapp_db;
 
-DROP TABLE IF EXISTS order;
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS order_detail;
-DROP TABLE IF EXISTS mineral;
 DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS mineral;
+DROP TABLE IF EXISTS order_mineral;
+DROP TABLE IF EXISTS person;
 
-CREATE TABLE `user` (
+
+
+CREATE TABLE person (
 	-- ptete pas utile ici le not null
-	id varchar(255) not null auto_increment primary key,
+	id INT not null auto_increment primary key,
     last_name varchar(255) not null,
     first_name varchar(255) not null,
     address varchar(255) not null,
@@ -19,28 +21,28 @@ CREATE TABLE `user` (
     birth_date int
 );
 
-CREATE TABLE `order` (
-	id varchar(255) not null auto_increment primary key,
-	date date
+CREATE TABLE order_mineral (
+	id INT not null auto_increment primary key,
+	order_date date
 );
 
 -- command line
 -- n'a pas une double clé primaire car chaq nouv commande avce ce command_id est juste un incrémentation à quantity
-CREATE TABLE `order_detail` (
+CREATE TABLE order_detail (
 	quantity int not null,
-	order_id varchar(255) references order(id),
+	order_mineral_id varchar(255) references order_mineral(id),
     mineral_id varchar(255) references mineral(id),
-    primary_key (order_id, mineral_id)
+    PRIMARY KEY pk_order_detail (order_mineral_id, mineral_id)
 );
 
-CREATE TABLE `mineral` (
-	id varchar(255) not null auto_increment primary key,
+CREATE TABLE mineral (
+	id INT not null auto_increment primary key,
     name_fr varchar(255) not null,
     name_en varchar(255) not null,
     price float not null
 );
 
-CREATE TABLE `category` (
+CREATE TABLE category (
 	-- couleur, transparence, densite (1-6), type geologique
 	id int not null auto_increment primary key,
 	name_fr varchar(255) not null,
