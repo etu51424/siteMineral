0f8d697aa8650c2f8e0be5d976c5f29bdece8adc
Demeure.Corentin <160240483+etu51424@users.noreply.github.com>
2024-11-11 14:53:41 +0100

    corrections db sur order detail, category, person

--- database/initDB.sql
+++ database/initDB.sql
@@ -6,9 +6,9 @@ DROP TABLE IF EXISTS order_detail;
 DROP TABLE IF EXISTS mineral;
 DROP TABLE IF EXISTS category;
 
-CREATE TABLE `person` (
+CREATE TABLE `user` (
 	-- ptete pas utile ici le not null
-	user_id varchar(255) not null auto_increment primary key,
+	id varchar(255) not null auto_increment primary key,
     last_name varchar(255) not null,
     first_name varchar(255) not null,
     address varchar(255) not null,
@@ -20,27 +20,29 @@ CREATE TABLE `person` (
 );
 
 CREATE TABLE `order` (
-	order_id varchar(255) not null auto_increment primary key,
+	id varchar(255) not null auto_increment primary key,
 	date date
 );
 
 -- command line
 -- n'a pas une double clé primaire car chaq nouv commande avce ce command_id est juste un incrémentation à quantity
 CREATE TABLE `order_detail` (
-	quantity int,
-	command_id varchar(255) not null auto_increment primary key
+	quantity int not null,
+	order_id varchar(255) references order(id),
+    mineral_id varchar(255) references mineral(id),
+    primary_key (order_id, mineral_id)
 );
 
 CREATE TABLE `mineral` (
-	mineral_id varchar(255) not null auto_increment primary key,
+	id varchar(255) not null auto_increment primary key,
     name_fr varchar(255) not null,
-    name_fr varchar(255) not null,
+    name_en varchar(255) not null,
     price float not null
 );
 
 CREATE TABLE `category` (
 	-- couleur, transparence, densite (1-6), type geologique
-	category_id int primary key,
+	id int not null auto_increment primary key,
 	name_fr varchar(255) not null,
     name_en varchar(255) not null
 );
