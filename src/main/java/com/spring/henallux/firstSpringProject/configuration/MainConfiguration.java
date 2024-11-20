a902833ab0bbe9ac316b4bc0fa07665b683c6a33
Xavier <etu51971@henallux.be>
2024-11-20 17:12:04 +0100

    init p2

---
+++ src/main/java/com/spring/henallux/firstSpringProject/configuration/MainConfiguration.java
@@ -0,0 +1,17 @@
+package com.spring.henallux.firstSpringProject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class MainConfiguration  {
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/java/com/spring/henallux/firstSpringProject/model/Category.java
@@ -0,0 +1,9 @@
+package com.spring.henallux.firstSpringProject.model;
+
+public class Category {
+    private int id;
+
+    public Category(){
+
+    }
+}
