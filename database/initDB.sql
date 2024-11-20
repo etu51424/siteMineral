361a055b874fa013925f3c09252b715589e79daf
xavierDujardinn <160240062+xavierDujardinn@users.noreply.github.com>
2024-11-20 13:02:36 +0100

    modif

--- database/initDB.sql
+++ database/initDB.sql
@@ -8,31 +8,38 @@ DROP TABLE IF EXISTS order_mineral;
 DROP TABLE IF EXISTS person;
 
 CREATE TABLE person (
-	-- ptete pas utile ici le not null
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone_number VARCHAR(255) NOT NULL,
-    country VARCHAR(255),
     gender CHAR,
-    birth_date DATE
-    
+    birth_date DATE,
+    password VARCHAR(255) NOT NULL,
+    authorities VARCHAR(25) NOT NULL DEFAULt 'ROLE_USER',
+    non_expired BOOL DEFAULT NULL,
+    non_locked BOOL DEFAULT NULL,
+    credentials_non_expired BOOL DEFAULT NULL,
+    enabled BOOL DEFAULT NULL,
+
     CHECK (birth_date >= '1900-01-01'),
-    CHECK (gender in ('M', 'F', 'X'))
+    CHECK (gender in ('M', 'F', 'X')),
+    CHECK (authorities in ('ROLE_USER', 'ROLE_ADMIN'))
 );
 
 CREATE TABLE order_mineral (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    person_id INT NOT NULL REFERENCES person(id),
+        person_id INT NOT NULL REFERENCES person(id),
 	order_date DATE,
+	is_paid BOOL NOT NULL DEFAULT false,
     
     CHECK (order_date >= '2024-01-01')
 );
 
 CREATE TABLE order_detail (
-	order_mineral_id VARCHAR(255) NOT NULL REFERENCES order_mineral(id),
+    order_mineral_id VARCHAR(255) NOT NULL REFERENCES order_mineral(id),
     mineral_id VARCHAR(255) NOT NULL REFERENCES mineral(id),
     quantity INT NOT NULL,
     PRIMARY KEY pk_order_detail (order_mineral_id, mineral_id),
@@ -42,13 +49,13 @@ CREATE TABLE order_detail (
 
 CREATE TABLE category (
 	--  type geologique
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name_fr VARCHAR(255) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name_fr VARCHAR(255) NOT NULL,
     name_en VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE mineral (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name_fr VARCHAR(255) NOT NULL,
     name_en VARCHAR(255) NOT NULL,
     color_fr VARCHAR(255) NOT NULL,
